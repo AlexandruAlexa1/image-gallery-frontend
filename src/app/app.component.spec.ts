5cf2cbe1cacfd5e0b2fb6e63e0715a10e21a6f47
Alexandru Alexa <x.alx@yahoo.com>
2025-10-31 18:13:45 +0200

    test: update title assertion in AppComponent tests to match expected output

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, image-gallery-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Image Gallery');
   });
 });
